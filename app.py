4b3ecb1ea836b58fb7bb6ff84a09997e04ca160c
vincentmuriuki <vincentmuriuki42@gmail.com>
2019-01-08 12:48:04 +0300

    modifications [Finishes #163051791]

--- app.py
+++ app.py
@@ -3,3 +3,4 @@
 def __init__(self):
     if '__app__' == '__main__':
         app.run(debug=True)
+
---
+++ run.py
@@ -0,0 +1,7 @@
+
+class Operations():
+    def __init__(self, name):
+        orders = orders()
+
+    def save(self):
+        orders = self.save()
